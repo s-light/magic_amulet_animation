ac37b53c8d64c1b68ed1fa8afb005b2c2d30b071
s-light <github@s-light.eu>
2018-11-28 00:02:55 +0100

    some more tests and cleanup

--- circuitpython/TLC5957_test/main.py
+++ circuitpython/TLC5957_test/main.py
@@ -5,10 +5,14 @@ Magic Amulet Animation.
 
 ported from / based on animation.ino
 Enjoy the colors :-)
+
+TODO: port it :-)
 """
 
 import board
-import pulseio
+
+import adafruit_fancyled.adafruit_fancyled as fancyled
+import animation
 
 
 ##########################################
@@ -19,16 +23,6 @@ print(
     (42 * '*') + "\n" +
     "\n"
 )
-
-##########################################
-print(42 * '*')
-print("test for pwm")
-
-# on the ItsyBitsy M4 EXPRESS on pin D9 the maximum frequency is about 6MHz?!
-gsclk_freqency = (6000 * 1000)  # 6MHz
-gsclk = pulseio.PWMOut(
-    board.D9, duty_cycle=(2 ** 15), frequency=gsclk_freqency)
-print("gsclk.frequency: {:}MHz".format(gsclk.frequency / (1000*1000)))
 
 ##########################################
 # helper function
@@ -42,12 +36,13 @@ offset = 0
 print(42 * '*')
 print("rainbow loop")
 while True:
-    for i in range(num_leds):
+    for i in range(animation.num_leds):
         # Load each pixel's color from the palette using an offset, run it
         # through the gamma function, pack RGB value and assign to pixel.
-        color = fancyled.palette_lookup(palette, offset + i / num_leds)
+        color = fancyled.palette_lookup(
+            animation.palette, offset + i / animation.num_leds)
         color = fancyled.gamma_adjust(color, brightness=0.01)
-        pixels[i] = color
-    pixels.show()
+        animation.pixels[i] = color
+    animation.pixels.show()
 
     offset += 0.01  # Bigger number = faster spin
--- circuitpython/TLC5957_test/test_pwm.py
+++ circuitpython/TLC5957_test/test_pwm.py
@@ -8,10 +8,12 @@ import pulseio
 print(42 * '*')
 print("test for pwm")
 
-# on the ItsyBitsy M4 EXPRESS on pin D9 the maximum frequency is about 6MHz?!
-freqency = (6000 * 1000)  # 6MHz
-pwm = pulseio.PWMOut(
-    board.D9, duty_cycle=(2 ** 15), frequency=freqency)
+# maximum frequency is currently hardcoded to 6MHz
+# https://github.com/adafruit/circuitpython/blob/master/ports/atmel-samd/common-hal/pulseio/PWMOut.c#L119
+pwm = pulseio.PWMOut(board.D9, duty_cycle=(2 ** 15), frequency=(6000 * 1000))
+# freqency = (6000 * 1000)  # 6MHz
+# pwm = pulseio.PWMOut(
+#     board.D9, duty_cycle=(2 ** 15), frequency=freqency)
 print("gsclk.frequency: {:}MHz".format(pwm.frequency / (1000*1000)))
 
 ##########################################
@@ -21,3 +23,24 @@ print("loop")
 while True:
     time.sleep(10)
     print(time)
+
+#
+# ```
+# Adafruit CircuitPython 3.1.1 on 2018-11-02; Adafruit ItsyBitsy M4 Express with samd51g19
+# >>> import board
+# >>> import pulseio
+# >>> pwm = pulseio.PWMOut(board.D9, duty_cycle=(2 ** 15), frequency=(6001 * 1000))
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# ValueError: Invalid PWM frequency
+# ```
+#
+# ```
+# Adafruit CircuitPython 3.1.1 on 2018-11-02; Adafruit ItsyBitsy M4 Express with samd51g19
+# >>> import board
+# >>> import pulseio
+# >>> pwm = pulseio.PWMOut(board.D9, duty_cycle=(2 ** 15), frequency=(6000 * 1000))
+# >>> pwm.frequency
+# 6000000
+# >>>
+# ```
