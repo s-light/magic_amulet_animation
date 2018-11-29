a625419675c0b43bcd1e81b4fb34344350462852
s-light <github@s-light.eu>
2018-11-29 10:14:20 +0100

    clean up animation.py

--- circuitpython/TLC5957_test/animation.py
+++ circuitpython/TLC5957_test/animation.py
@@ -1,9 +1,11 @@
 """TLC5957 & FancyLED."""
 
 __doc__ = """
-TLC5957 & FancyLED.
+animation.py - TLC5957 & FancyLED & 2D Array / Mapping.
 
-this is an example for combining the TLC5957 library with FancyLED.
+this is sub file for the magic amulet animation things.
+it combines the TLC5957 library with FancyLED and 2D Array / Mapping.
+
 Enjoy the colors :-)
 """
 
@@ -16,14 +18,16 @@ import digitalio
 import slight_tlc5957
 import adafruit_fancyled.adafruit_fancyled as fancyled
 
+
 ##########################################
-print(
-    "\n" +
-    (42 * '*') + "\n" +
-    __doc__ + "\n" +
-    (42 * '*') + "\n" +
-    "\n"
-)
+if __name__ == '__main__':
+    print(
+        "\n" +
+        (42 * '*') + "\n" +
+        __doc__ + "\n" +
+        (42 * '*') + "\n" +
+        "\n"
+    )
 
 ##########################################
 print(42 * '*')
@@ -52,7 +56,9 @@ latch.direction = digitalio.Direction.OUTPUT
 ##########################################
 print(42 * '*')
 print("define pixel array / init TLC5957")
-num_leds = 16
+rows = 4
+cols = 4
+num_leds = rows * cols
 pixels = slight_tlc5957.TLC5957(
     spi=spi,
     latch=latch,
@@ -62,13 +68,54 @@ pixels = slight_tlc5957.TLC5957(
     spi_miso=spi_miso,
     pixel_count=num_leds)
 
-print("pixel_count", pixels.pixel_count)
-print("chip_count", pixels.chip_count)
-print("channel_count", pixels.channel_count)
+pixel_map = [
+    [15, 14, 13, 12],
+    [11, 10,  9,  8],
+    [7,   6,  5,  4],
+    [3,   2,  1,  0],
+]
+
+print(
+    "Pixels & Mapping:\n"
+    "  rows           {rows:>2}\n"
+    "  cols           {cols:>2}\n"
+    "  pixel_count    {pixel_count:>2}\n"
+    "  chip_count     {chip_count:>2}\n"
+    "  channel_count  {channel_count:>2}\n"
+    "".format(
+        rows=rows,
+        cols=cols,
+        pixel_count=pixels.pixel_count,
+        chip_count=pixels.chip_count,
+        channel_count=pixels.channel_count,
+    )
+)
 
 
 ##########################################
 # helper function
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    """Map Value from one range to another."""
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def map_range_int(x, in_min, in_max, out_min, out_max):
+    """Map Value from one range to another."""
+    return int(
+        (x - in_min) * (out_max - out_min)
+        //
+        (in_max - in_min) + out_min
+    )
+
+
+##########################################
+# mapping function
+
+def get_pixel_index_from_row_col(row, col):
+    """Get pixel_index from row and column index."""
+    pixel_index = pixel_map[row][col]
+    return pixel_index
 
 
 ##########################################
@@ -81,3 +128,77 @@ palette = [
     fancyled.CRGB(0.0, 0.0, 1.0),  # Blue
     fancyled.CRGB(0.5, 0.0, 0.5),  # Magenta
 ]
+
+
+##########################################
+# test functions
+
+def test_set_corners_to_colors():
+    """Test Function: Set all 4 corners to different collors."""
+    print(42 * '*')
+    print("set corners to colors")
+    pixels[get_pixel_index_from_row_col(0, 0)] = (1.0, 0.5, 0.0)
+    pixels[get_pixel_index_from_row_col(0, 3)] = (0.5, 0.0, 1.0)
+    pixels[get_pixel_index_from_row_col(3, 0)] = (0.1, 0.5, 0.0)
+    pixels[get_pixel_index_from_row_col(3, 3)] = (0.0, 0.5, 1.0)
+    pixels.show()
+
+
+def test_set_2d_colors():
+    """Test Function: Set all LEDs to 2D color-range."""
+    print("set color range")
+    for x in range(cols):
+        # xN = x / cols
+        xN = map_range_int(x, 0, cols, 1, 100)
+        for y in range(rows):
+            # yN = y / rows
+            yN = map_range_int(y, 0, rows, 1, 100)
+            # print(
+            #     "x: {:>2} xN: {:>2} "
+            #     "y: {:>2} yN: {:>2} "
+            #     "pixel_index: {:>2}".format(
+            #         x, xN,
+            #         y, yN,
+            #         get_pixel_index_from_row_col(x, y)
+            #     )
+            # )
+            pixels[get_pixel_index_from_row_col(x, y)] = (xN, yN, 0)
+
+    pixels.show()
+
+
+def test_loop_2d_colors():
+    """Test Function: Set all LEDs to 2D color-range."""
+    # Positional offset for blue part
+    offset = 0
+    print(42 * '*')
+    print("loop")
+    while True:
+        offsetN = map_range_int(offset, 0.0, 1.0, 1, 200)
+        for x in range(cols):
+            xN = map_range_int(x, 0, cols, 1, 500)
+            for y in range(rows):
+                yN = map_range_int(y, 0, rows, 1, 500)
+                pixels[get_pixel_index_from_row_col(x, y)] = (xN, yN, offsetN)
+        pixels.show()
+        offset += 0.01  # Bigger number = faster spin
+        if offset > 1.0:
+            offset = 0
+
+
+def test_main():
+    """Test Main."""
+    import time
+
+    test_set_corners_to_colors()
+    time.sleep(10)
+    test_set_2d_colors()
+    time.sleep(10)
+    test_loop_2d_colors()
+
+
+##########################################
+# main loop
+
+if __name__ == '__main__':
+    test_main()
