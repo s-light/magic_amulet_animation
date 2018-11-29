9759b45916269a7eaec3f43e4b729cc979b2f93c
s-light <github@s-light.eu>
2018-11-29 10:13:50 +0100

    cleaned and extended test_pwm.py

--- circuitpython/TLC5957_test/test_pwm.py
+++ circuitpython/TLC5957_test/test_pwm.py
@@ -1,46 +1,47 @@
 """Test for PWM frequency."""
-import time
 
+import supervisor
 import board
 import pulseio
 
 ##########################################
 print(42 * '*')
-print("test for pwm")
+print("test for pwm frequency")
 
 # maximum frequency is currently hardcoded to 6MHz
 # https://github.com/adafruit/circuitpython/blob/master/ports/atmel-samd/common-hal/pulseio/PWMOut.c#L119
-pwm = pulseio.PWMOut(board.D9, duty_cycle=(2 ** 15), frequency=(6000 * 1000))
-# freqency = (6000 * 1000)  # 6MHz
-# pwm = pulseio.PWMOut(
-#     board.D9, duty_cycle=(2 ** 15), frequency=freqency)
-print("gsclk.frequency: {:}MHz".format(pwm.frequency / (1000*1000)))
+freqency = (6000 * 1000)  # 6MHz
+pwm = pulseio.PWMOut(
+    board.D9, duty_cycle=(2 ** 15), frequency=freqency)
+print("pwm.frequency: {:}MHz".format(pwm.frequency / (1000*1000)))
 
 ##########################################
 # main loop
 print(42 * '*')
 print("loop")
-while True:
-    time.sleep(10)
-    print(time)
 
-#
-# ```
-# Adafruit CircuitPython 3.1.1 on 2018-11-02; Adafruit ItsyBitsy M4 Express with samd51g19
-# >>> import board
-# >>> import pulseio
-# >>> pwm = pulseio.PWMOut(board.D9, duty_cycle=(2 ** 15), frequency=(6001 * 1000))
-# Traceback (most recent call last):
-#   File "<stdin>", line 1, in <module>
-# ValueError: Invalid PWM frequency
-# ```
-#
-# ```
-# Adafruit CircuitPython 3.1.1 on 2018-11-02; Adafruit ItsyBitsy M4 Express with samd51g19
-# >>> import board
-# >>> import pulseio
-# >>> pwm = pulseio.PWMOut(board.D9, duty_cycle=(2 ** 15), frequency=(6000 * 1000))
-# >>> pwm.frequency
-# 6000000
-# >>>
-# ```
+if supervisor.runtime.serial_connected:
+    print("you can change the pwm.frequency:")
+    print("new frequency in MHz: ")
+while True:
+    if supervisor.runtime.serial_bytes_available:
+        new_value = input()
+        new_freq_Hz = pwm.frequency
+        try:
+            new_freq_Hz = int(float(new_value) * 1000 * 1000)
+        except Exception as e:
+            print("Exception: ", e)
+        print(
+            "calculated frequency: {:}MHz (= {:}Hz)".format(
+                new_freq_Hz / (1000*1000),
+                new_freq_Hz
+            )
+        )
+        pwm.frequency = new_freq_Hz
+        # reset duty_cycle to half
+        # https://circuitpython.readthedocs.io/en/latest/shared-bindings/pulseio/PWMOut.html#pulseio.PWMOut.duty_cycle
+        pwm.duty_cycle = 0x7fff
+        # pwm.duty_cycle = (2 ** 15)
+        print("pwm.frequency: {:}MHz".format(pwm.frequency / (1000*1000)))
+        # prepare new input
+        print("\nenter new frequency in MHz: ")
