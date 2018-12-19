ac3b590788727a4f6db3da85892f211bd66ac232
s-light <github@s-light.eu>
2018-12-19 12:25:03 +0100

    some basic rainbow animations

--- circuitpython/TLC5957_test/animation.py
+++ circuitpython/TLC5957_test/animation.py
@@ -45,7 +45,8 @@ print("init bitbangio.SPI")
 spi = bitbangio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
 # on the ItsyBitsy M4 EXPRESS on pin D9 the maximum frequency is about 6MHz?!
-gsclk_freqency = (6000 * 1000)  # 6MHz
+# gsclk_freqency = (6 * 1000 * 1000)  # 6MHz
+gsclk_freqency = (20 * 1000 * 1000)  # 10MHz
 gsclk = pulseio.PWMOut(
     board.D9, duty_cycle=(2 ** 15), frequency=gsclk_freqency)
 print("gsclk.frequency: {:}MHz".format(gsclk.frequency / (1000*1000)))
@@ -56,9 +57,11 @@ latch.direction = digitalio.Direction.OUTPUT
 ##########################################
 print(42 * '*')
 print("define pixel array / init TLC5957")
-rows = 4
-cols = 4
-num_leds = rows * cols
+LEDBoard_count = 3
+LEDBoard_row_count = 4
+LEDBoard_col_count = 4
+pixel_col_count = LEDBoard_count * LEDBoard_col_count
+pixel_count = pixel_col_count * LEDBoard_row_count
 pixels = slight_tlc5957.TLC5957(
     spi=spi,
     latch=latch,
@@ -66,30 +69,124 @@ pixels = slight_tlc5957.TLC5957(
     spi_clock=spi_clock,
     spi_mosi=spi_mosi,
     spi_miso=spi_miso,
-    pixel_count=num_leds)
+    pixel_count=pixel_count)
 
-pixel_map = [
+# How to map a 2D-Pixel Array to the needed TLC5957 sending order:
+#
+# physically the pixels are orderd this way on the board:
+# physical_map = [
+#     [ 0,  1,  2,  3],
+#     [ 4,  5,  6,  7],
+#     [ 8,  9, 10, 11],
+#     [12, 13, 14, 15],
+# ]
+# but we need to reverse this order for the send data.
+# this results in
+# pixel_map = [
+#     [15, 14, 13, 12],
+#     [11, 10,  9,  8],
+#     [ 7,  6,  5,  4],
+#     [ 3,  2,  1,  0],
+# ]
+# if we have more than one chip it gets a little bit complicated.
+# we need to first send all physical last pixels in one row.
+# than the next and so on..
+# so if we have a physical map with 2*16 LEDs:
+# physical_map = [
+#      CHIP_1            CHIP_2
+#     [ 0,  1,  2,  3,    0,  1,  2,  3],
+#     [ 4,  5,  6,  7,    4,  5,  6,  7],
+#     [ 8,  9, 10, 11,    8,  9, 10, 11],
+#     [12, 13, 14, 15,   12, 13, 14, 15],
+# ]
+# this results in this pixel-sending order:
+# pixel_map = [
+#      CHIP_1            CHIP_2
+#     [31, 29, 27, 25,   30, 28, 26, 24],
+#     [23, 21, 19, 17,   22, 20, 18, 16],
+#     [15, 13, 11,  9,   14, 12, 10,  8],
+#     [ 7,  5,  3,  1,    6,  4,  2,  0],
+# ]
+
+
+LEDBoard_map = [
     [15, 14, 13, 12],
     [11, 10,  9,  8],
     [7,   6,  5,  4],
     [3,   2,  1,  0],
 ]
 
-print(
-    "Pixels & Mapping:\n"
-    "  rows           {rows:>2}\n"
-    "  cols           {cols:>2}\n"
-    "  pixel_count    {pixel_count:>2}\n"
-    "  chip_count     {chip_count:>2}\n"
-    "  channel_count  {channel_count:>2}\n"
-    "".format(
-        rows=rows,
-        cols=cols,
-        pixel_count=pixels.pixel_count,
-        chip_count=pixels.chip_count,
-        channel_count=pixels.channel_count,
+# create empty 2d list
+# this will be filled by the initialisation for all chips/pcbs
+pixel_map = [
+    [0 for c in range(pixel_col_count)] for r in range(LEDBoard_row_count)
+]
+# print(pixel_map)
+
+
+def pixel_map_fill():
+    """."""
+    index_offset = 0
+
+    print(
+        "|----------index----------|-----------map----------|\n"
+        "| board | row | col | cwb | offset | board | pixel |"
     )
-)
+    for board_index in range(LEDBoard_count):
+        for row_index in range(LEDBoard_row_count):
+            for col_index in range(LEDBoard_col_count):
+                col_index_w_board = col_index * board_index
+                # set new pixel_index
+                pixel_map[row_index][col_index_w_board] = (
+                    index_offset + LEDBoard_map[row_index][col_index])
+                # print debug things
+                print(
+                    "| "
+                    "{board:^5} | "
+                    "{row:^3} | "
+                    "{col:^3} | "
+                    "{col_w_board:^3} | "
+                    "{index_offset:^6} | "
+                    "{board_map:^5} | "
+                    "{pixel_map:^5} | "
+                    "".format(
+                        board=board_index,
+                        col=col_index,
+                        row=row_index,
+                        col_w_board=col_index_w_board,
+                        index_offset=index_offset,
+                        board_map=LEDBoard_map[row_index][col_index],
+                        pixel_map=pixel_map[row_index][col_index_w_board],
+                    )
+                )
+
+        # prepare next board
+        index_offset += pixels.CHIP_LED_COUNT
+    print("|--------------------------------------------------|")
+
+# pixel_map_fill()
+
+
+def pixel_map_print_information():
+    """Print all Information regarding pixel_map."""
+    print(
+        "Pixels & Mapping:\n"
+        "  LEDBoard_count {LEDBoard_count:>2}\n"
+        "  LEDBoard_row_count           {LEDBoard_row_count:>2}\n"
+        "  LEDBoard_col_count           {LEDBoard_col_count:>2}\n"
+        "  pixel_count    {pixel_count:>2}\n"
+        "  chip_count     {chip_count:>2}\n"
+        "  channel_count  {channel_count:>2}\n"
+        "".format(
+            LEDBoard_count=LEDBoard_count,
+            LEDBoard_row_count=LEDBoard_row_count,
+            LEDBoard_col_count=LEDBoard_col_count,
+            pixel_count=pixels.pixel_count,
+            chip_count=pixels.chip_count,
+            channel_count=pixels.channel_count,
+        )
+    )
+    print(pixel_map)
 
 
 ##########################################
@@ -147,12 +244,12 @@ def test_set_corners_to_colors():
 def test_set_2d_colors():
     """Test Function: Set all LEDs to 2D color-range."""
     print("set color range")
-    for x in range(cols):
-        # xN = x / cols
-        xN = map_range_int(x, 0, cols, 1, 100)
-        for y in range(rows):
-            # yN = y / rows
-            yN = map_range_int(y, 0, rows, 1, 100)
+    for x in range(LEDBoard_col_count):
+        # xN = x / LEDBoard_col_count
+        xN = map_range_int(x, 0, LEDBoard_col_count, 1, 100)
+        for y in range(LEDBoard_row_count):
+            # yN = y / LEDBoard_row_count
+            yN = map_range_int(y, 0, LEDBoard_row_count, 1, 100)
             # print(
             #     "x: {:>2} xN: {:>2} "
             #     "y: {:>2} yN: {:>2} "
@@ -175,10 +272,10 @@ def test_loop_2d_colors():
     print("loop")
     while True:
         offsetN = map_range_int(offset, 0.0, 1.0, 1, 200)
-        for x in range(cols):
-            xN = map_range_int(x, 0, cols, 1, 500)
-            for y in range(rows):
-                yN = map_range_int(y, 0, rows, 1, 500)
+        for x in range(LEDBoard_col_count):
+            xN = map_range_int(x, 0, LEDBoard_col_count, 1, 500)
+            for y in range(LEDBoard_row_count):
+                yN = map_range_int(y, 0, LEDBoard_row_count, 1, 500)
                 pixels[get_pixel_index_from_row_col(x, y)] = (xN, yN, offsetN)
         pixels.show()
         offset += 0.01  # Bigger number = faster spin
--- circuitpython/TLC5957_test/main.py
+++ circuitpython/TLC5957_test/main.py
@@ -9,7 +9,9 @@ Enjoy the colors :-)
 TODO: port it :-)
 """
 
-import board
+import time
+
+# import board
 
 import adafruit_fancyled.adafruit_fancyled as fancyled
 import animation
@@ -28,21 +30,41 @@ print(
 # helper function
 
 
+##########################################
+# function
+
 # Positional offset into color palette to get it to 'spin'
 offset = 0
 
-##########################################
-# main loop
-print(42 * '*')
-print("rainbow loop")
-while True:
-    for i in range(animation.num_leds):
+
+def main_animation():
+    """Animation example."""
+    global offset
+    for i in range(animation.pixel_count):
         # Load each pixel's color from the palette using an offset, run it
         # through the gamma function, pack RGB value and assign to pixel.
         color = fancyled.palette_lookup(
-            animation.palette, offset + i / animation.num_leds)
-        color = fancyled.gamma_adjust(color, brightness=0.01)
+            animation.palette, offset + i / animation.pixel_count)
+        color = fancyled.gamma_adjust(color, brightness=0.1)
         animation.pixels[i] = color
-    animation.pixels.show()
+        animation.pixels.show()
 
-    offset += 0.01  # Bigger number = faster spin
+        offset += 0.01  # Bigger number = faster spin
+
+
+##########################################
+# main
+if __name__ == '__main__':
+    print(42 * '*')
+    time.sleep(5)
+    # print("\n")
+    # animation.pixel_map_print_information()
+    print("\n")
+    animation.pixel_map_fill()
+    print("\n")
+    # animation.pixel_map_print_information()
+    # print("\n")
+    print(42 * '*')
+    print("rainbow loop")
+    while True:
+        main_animation()
